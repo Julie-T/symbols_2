914415af2f1cdadf07c8bd1b48d6bb0da59f3823
alxmkh <walx404@gmail.com>
2022-08-13 09:43:06 +0300

    Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CI Test
+
+[![Build status](https://ci.appveyor.com/api/projects/status/4gv87gxqltgvjrm5?svg=true)](https://ci.appveyor.com/project/Julie-T/symbols-2)
--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,28 @@
-// TODO: write your code here
-import sum from './basic';
+const arr = [];
 
-console.log('worked');
+function Person(name, type, health, level, attack, defence) {
+  this.name = name;
+  this.type = type;
+  this.health = health || 100;
+  this.level = level || 1;
+  this.attack = attack;
+  this.defence = defence;
+  this.add = () => {
+    arr.push(this);
+  };
+}
 
-console.log(sum([1, 2]));
+const henry = new Person('Лучник', 'Bowman', 50, 1, 40, 10);
+const phill = new Person('Маг', 'Wizard', 40, 1, 30, 20);
+
+henry.add();
+phill.add();
+let i = -1;
+
+export default class Team2 {
+  static* iterator() {
+    i += 1;
+    yield arr[i];
+    return arr[arr.length - 1];
+  }
+}
